be79e6fff88346462a5fb26ebc90c9ef09ab7d1c
Max New <maxsnew@gmail.com>
2014-06-03 18:09:20 -0500

    Pairing and some other bits

--- EnumCheck/Enum.elm
+++ EnumCheck/Enum.elm
@@ -1,6 +1,7 @@
 module EnumCheck.Enum where
 
 import Array
+import Either (..)
 import Native.Error
 
 import EnumCheck.ExtNat (..)
@@ -9,6 +10,9 @@ type Nat = Int
 type Enum a = { size : ExtNat 
               , fromNat : Nat -> a
               }
+
+toList : Enum a -> [a]
+toList e = map e.fromNat [0 .. toInt e.size - 1]
 
 fromNat : Nat -> Enum a -> a
 fromNat n e = if | n < 0            -> Native.Error.raise "fromNat only takes nats"
@@ -35,7 +39,7 @@ takeE n es = { size = nat n
              }
 
 dropE : Nat -> Enum a -> Enum a
-dropE n es = { size = es.size - n
+dropE n es = { size = es.size -! nat n
              , fromNat = es.fromNat . (\x -> x + n)
              }
 
@@ -46,26 +50,58 @@ appendE xs ys = { size = xs.size +! ys.size
                               else ys.fromNat (n - toInt xs.size)
                 }
 
+quotRem x y = (x `div` y, x `mod` y)
+
 orE : Enum a -> Enum a -> Enum a
 orE xs ys = 
     let newSize = xs.size +! ys.size
     in 
-      if | xs.size == isInf ys -> 
+      if | xs.size == ys.size -> 
              { size = newSize
-             , fromNat n = let q = n `div` 2
-                               r = n `mod` 2
+             , fromNat n = let (q,r) = quotRem n 2
                                e = if r == 0 then xs else ys
                            in e.fromNat q
              }
          | otherwise ->
              let (lessE, bigE, flipped) = 
+                     
                      if | xs.size <=! ys.size -> (xs, ys, False)
                         | otherwise -> (ys, xs, True)
-                 (belowBigE, aboveE) = splitE lessE.size bigE
+                 (belowBigE, aboveE) = splitE (toInt lessE.size) bigE
                  belowE = if flipped
                           then orE belowBigE lessE
                           else orE lessE belowBigE
              in appendE belowE aboveE
+
+eitherE : Enum a -> Enum b -> Enum (Either a b)
+eitherE l r = orE (mapE Left l) (mapE Right r)
+
+pickLess xs ys = if | xs.size <=! ys.size -> (xs, ys, False)
+                    | otherwise -> (ys, xs, True)
+
+pairE : Enum a -> Enum b -> Enum (a, b)
+pairE xs ys =
+    let newS = xs.size *! ys.size 
+    in 
+      if | xs.size == ys.size ->
+             let fromNat n = let flroot = floor . sqrt . toFloat <| n
+                                 flrootsq = flroot * flroot
+                                 diff = n - flrootsq
+                                 (n1, n2) = if n - flrootsq < flroot
+                                            then (diff, flroot)
+                                            else (flroot, diff - flroot)
+                             in (xs.fromNat n1, ys.fromNat n2)
+             in { size = newS, fromNat = fromNat }
+         | otherwise          ->
+             let firstLess = xs.size <=! ys.size
+                 lessSize = toInt <| if firstLess then xs.size else ys.size
+                 fromNat n =
+                     let (q, r) = quotRem n lessSize
+                         (n1, n2) = if firstLess 
+                                    then (r, q)
+                                    else (q, r)
+                     in (xs.fromNat n1, ys.fromNat n2)
+             in { size = newS , fromNat = fromNat }
 
 -- | Base enumerators
 natE : Enum Int
@@ -73,3 +109,6 @@ natE = { size = inf, fromNat = id }
 
 boolE : Enum Bool
 boolE = finE [True, False]
+
+ordE : Enum Order
+ordE = finE [LT, EQ, GT]
